efa72316e343cafdb1d4608b29f922e70dd6b945
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-06-19 18:56:42 -0500

    add svg to timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,50 @@
 import React, { useEffect, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 
-export const Timer = ({ initialTime, setTerminalActive }) => {
+const countdown = size => keyframes`
+  from {
+    stroke-dashoffset: 0px;
+  }
+  to {
+    stroke-dashoffset: ${2 * Math.PI * (size / 2)}px;
+  }
+`;
+
+const TimerContainer = styled.div`
+  position: relative;
+  height: ${props => props.size}px;
+  width: ${props => props.size}px;
+  margin: 0.5em 0;
+  text-align: center;
+`;
+
+const Countdown = styled.div`
+  color: white;
+  display: inline-block;
+  line-height: ${props => props.size}px;
+`;
+
+const SvgTimer = styled.svg`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  transform: rotateY(-180deg) rotateZ(-90deg);
+
+  circle {
+    stroke-dasharray: ${props => 2 * Math.PI * (props.size / 2)}px;
+    stroke-dashoffset: 0px;
+    stroke-linecap: round;
+    stroke-width: 4px;
+    stroke: white;
+    fill: none;
+    animation: ${props => countdown(props.size)} ${props => props.initialTime}s
+      linear forwards;
+  }
+`;
+
+export const Timer = ({ initialTime, setTerminalActive, size = 150 }) => {
   const [time, setTime] = useState(parseInt(initialTime));
 
   useEffect(() => {
@@ -27,11 +71,14 @@ export const Timer = ({ initialTime, setTerminalActive }) => {
       </span>
     );
   }
-
+  // cx, cy === radius r === radius - 2
   return (
-    <div>
-      Time Remaining:<span style={{ color: 'green' }}> {time}</span>
-    </div>
+    <TimerContainer size={size}>
+      <Countdown size={size}>{time}</Countdown>
+      <SvgTimer initialTime={initialTime} size={size}>
+        <circle r={size / 2 - 4} cx={size / 2} cy={size / 2} />
+      </SvgTimer>
+    </TimerContainer>
   );
 };
 
