ee5993dd2edc259072952818ee09184a75feadc1
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-06-19 10:52:49 -0500

    changed input to textarea

--- src/components/Console/ConsoleInput.js
+++ src/components/Console/ConsoleInput.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 
-const InputContainer = styled.form`
+const InputContainer = styled.div`
   display: flex;
 `;
 
-const Input = styled.input`
+const Textarea = styled.textarea`
   border: none;
   width: 100%;
   background-color: inherit;
@@ -13,6 +13,9 @@ const Input = styled.input`
   margin-left: 0.5em;
   font-family: inherit;
   font-size: inherit;
+  height: ${props => props.height}
+  resize: none;
+  padding: 0;
 
   &:focus {
     outline: none;
@@ -21,24 +24,42 @@ const Input = styled.input`
 
 const ConsoleInput = ({ runCommand }) => {
   const [input, setInput] = useState('');
+  const [inputHeight, setInputHeight] = useState('auto');
+  const consoleInput = useRef();
+  const consoleForm = useRef();
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const handleKeyDown = e => {
+    if (e.which === 13 && !e.shiftKey) {
+      e.preventDefault();
+      runCommand(input.trim());
+      setInput('');
+    }
 
-    runCommand(input);
+    setTimeout(() => {
+      setInputHeight('auto');
 
-    setInput('');
+      const { scrollHeight } = consoleInput.current;
+      setInputHeight(`${scrollHeight}px`);
+    }, 0);
+  };
+
+  const handleOnChange = e => {
+    setInput(e.target.value);
   };
 
   return (
-    <InputContainer onSubmit={handleSubmit}>
+    <InputContainer ref={consoleForm}>
       <label>> </label>
-      <Input
+      <Textarea
         value={input}
-        onChange={e => setInput(e.target.value)}
+        onChange={handleOnChange}
         id="console-input"
         autoFocus
         autoComplete="off"
+        rows="1"
+        ref={consoleInput}
+        onKeyDown={handleKeyDown}
+        height={inputHeight}
       />
     </InputContainer>
   );
--- src/components/MainConsole.js
+++ src/components/MainConsole.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import Console from './Console/';
 import { addToTerminalHistory } from '../store/actions';
+
+export const Timer = ({ initialTime }) => {
+  const [time, setTime] = useState(initialTime);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (time > 0) setTime(time => time - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  return <div>{time}</div>;
+};
 
 const MainConsole = ({ terminal, addToTerminalHistory }) => {
   const handleRunCommand = command => {
     addToTerminalHistory({ output: `> ${command}` });
 
     // console command logic goes here
-    addToTerminalHistory({ output: 'invalid command', color: '#ff5151' });
+    addToTerminalHistory({
+      output: 'invalid command',
+      color: '#ff5151',
+    });
   };
 
-  return <Console terminal={terminal} runCommand={handleRunCommand} />;
+  return (
+    <>
+      <Console terminal={terminal} runCommand={handleRunCommand} />
+    </>
+  );
 };
 
 const mapStateToProps = ({ terminal }) => {
