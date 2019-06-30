b549e1a3387da09cc0dd6b86ac52389a70875e00
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-06-30 13:21:52 -0500

    clear run console when run ends

--- src/store/reducers/runTerminalReducer/history.js
+++ src/store/reducers/runTerminalReducer/history.js
@@ -1,9 +1,14 @@
-import { ADD_TO_RUN_TERMINAL_HISTORY } from '../../actions';
+import {
+  ADD_TO_RUN_TERMINAL_HISTORY,
+  SWITCH_SCREEN_TO_MAIN_CONSOLE,
+} from '../../actions';
 
 export default (state = [], { type, payload }) => {
   switch (type) {
     case ADD_TO_RUN_TERMINAL_HISTORY:
       return [...state, payload];
+    case SWITCH_SCREEN_TO_MAIN_CONSOLE:
+      return [];
     default:
       return state;
   }
